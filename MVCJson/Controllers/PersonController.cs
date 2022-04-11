b57f113cb374b1cde6e101a043ba947e39961e9f
Sharif <sharifondevops@gmail.com>
2022-04-11 14:32:49 +0530

    Enahanced the Code to Add More users

--- MVCJson/Controllers/PersonController.cs
+++ MVCJson/Controllers/PersonController.cs
@@ -20,11 +20,31 @@ namespace MVCJson.Controllers
         public ActionResult Index(MPerson obj)
         {
             // Pass the "persondata" object for conversion to JSON string  
-            string jsondata = new JavaScriptSerializer().Serialize(obj);            
-            string path = Server.MapPath("~/App_Data/");
+            //string jsondata = new JavaScriptSerializer().Serialize(obj);            
+            //string path = Server.MapPath("~/App_Data/");
             // Write that JSON to txt file,  
-            System.IO.File.WriteAllText(path + "jsonResult.json", jsondata);
-            TempData["msg"] = "Json file Generated! Please check in App_Data folder";
+            //System.IO.File.WriteAllText(path + "jsonResult.json", jsondata);
+            //TempData["msg"] = "Json file Generated! Please check in App_Data folder";
+            //return RedirectToAction("Index");
+
+            var filePath = Server.MapPath("~/App_Data/") + "jsonResult.json";
+            // Read existing file json data if exist else its sets empty jsonstring
+            var jsonData = "";
+            if (System.IO.File.Exists(filePath))
+            {
+                jsonData = System.IO.File.ReadAllText(filePath);
+            }
+            //Deserializing the exising jsondata into a person list if data exist, else we are assigning a empty Person list.
+            var personList = new JavaScriptSerializer().Deserialize<List<MPerson>>(jsonData) ?? new List<MPerson>();
+            // Adding new person data to the list
+            personList.Add(obj);
+            // Pass the personlist object for conversion to JSON string  
+            jsonData = new JavaScriptSerializer().Serialize(personList);
+            //Writing to JSon file.
+            System.IO.File.WriteAllText(filePath, jsonData);
+
+            //Redirecting to HTTPGet Index method by passing the information.
+            TempData["msg"] = "Json file Created! Please check in App_Data folder.";
             return RedirectToAction("Index");
         }
     }
